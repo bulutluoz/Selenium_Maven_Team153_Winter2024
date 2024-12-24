2b35b93257ece6728cdbd2cb890399c3556c451d
bulutluoz <abbulutluoz@gmail.com>
2024-12-24 19:53:20 +0100

    Ders04 sonu

--- src/test/java/day04_xPath_cssLocator/C02_TextIleXPathKullanimi.java
+++ src/test/java/day04_xPath_cssLocator/C02_TextIleXPathKullanimi.java
@@ -17,32 +17,35 @@ public class C02_TextIleXPathKullanimi {
 
         //1- https://testotomasyonu.com/addremove/ adresine gidin
         driver.get("https://testotomasyonu.com/addremove/");
-
+        Thread.sleep(2000);
         //2- Add Element butonuna basin
-        driver.findElement(By.id("sub-btn"))
+
+        driver.findElement(By.xpath("//*[.='Add']"))
                 .click();
 
         //3- Remove butonu’nun gorunur oldugunu test edin
-        WebElement removeButonu = driver.findElement(By.xpath("//*[@class='remove-btn']"));
+        WebElement removeButonu = driver.findElement(By.xpath("//button[.='Remove']"));
 
         if (removeButonu.isDisplayed()){
             System.out.println("Remove butonu gorunme testi PASSED");
         }else System.out.println("Remove butonu gorunme testi FAILED");
-
+        Thread.sleep(2000);
         //4- Remove tusuna basin
         removeButonu.click();
 
         //5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
 
-        WebElement addRemoveYaziElementi = driver.findElement(By.tagName("h2"));
+        //WebElement addRemoveYaziElementi = driver.findElement(By.tagName("h2"));
 
-        //WebElement addRemoveYaziElementi = driver.findElement(By.xpath("//h2"));
+        WebElement addRemoveYaziElementi =
+                driver.findElement(By.xpath("//h2[.='Add/Remove Elements']"));
+
 
         if (addRemoveYaziElementi.isDisplayed()){
             System.out.println("Add Remove Yazisi gorunme testi PASSED");
         }else System.out.println("Add Remove Yazisi gorunme testi FAILED");
 
-
+        Thread.sleep(2000);
         driver.quit();
 
     }
